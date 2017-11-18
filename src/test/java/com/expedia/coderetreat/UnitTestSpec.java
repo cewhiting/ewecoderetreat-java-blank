d75f33842db7a6704dd0c8463317a6cd17133c65
Chad Whiting <cwhiting@expedia.com>
2017-11-18 05:41:00 -0800

    Updates to latest TestNG and removing abstract from UnitTestSpec to make it easier to use

--- src/test/java/com/expedia/coderetreat/UnitTestSpec.java
+++ src/test/java/com/expedia/coderetreat/UnitTestSpec.java
@@ -2,6 +2,16 @@ package com.expedia.coderetreat;
 
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertEquals;
+
 @Test(groups = "unit")
-public abstract class UnitTestSpec {
+public class UnitTestSpec {
+
+    @Test
+    public void sampleTest() {
+        final boolean actual = true;
+        final boolean expected = true;
+        assertEquals(actual, expected);
+    }
+
 }
